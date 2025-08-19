f84137373540a55cb4191d2a3de462e68a767036
Alan De franceschi <ade-fran@f6r9s1.clusters.42paris.fr>
2025-08-19 17:53:34 +0200

    add jokes

--- srcs/Bot.cpp
+++ srcs/Bot.cpp
@@ -3,7 +3,21 @@
 
 Bot::Bot(void): _nick("bot")
 {
-    _jokes.push_back("HI");
+    std::string joke1 = "Pourquoi les mammouths ont-ils disparu ?   Reponse: Parce qu'il n'y avait plus de pappouths ;D\r\n";
+    std::string joke2 = "Une petite fille fait de la balancoire et tombe, pourquoi ?   Reponse: Parce qu'elle n'a pas de bras ;D\r\n";
+    std::string joke3 = "Pourquoi est-ce dit-on que les bretons sont tous freres et soeurs ?   Reponse: Parce qu'il n'ont Quimper ;D\r\n";
+    std::string joke4 = "Que faisaient les dinosaures quand ils n'arrivaient pas a prendre une decision ?   Reponse: Ils faisaient un Tirageosaure ;D\r\n";
+    std::string joke5 = "Qu'est-ce qu'un canife ?   Reponse: Un petit fien ;D\r\n";
+    std::string joke6 = "Quel est le moyen de transport prefere des dinosaures ?   Reponse: Le velociraptor ;D\r\n";
+    std::string joke7 = "Qu'est-ce qui est jaune et qui attend ?   Reponse: C'est JONATHAN !!!!! ;D\r\n";
+    _jokes.push_back(joke1);
+    _jokes.push_back(joke2);
+    _jokes.push_back(joke3);
+    _jokes.push_back(joke4);
+    _jokes.push_back(joke5);
+    _jokes.push_back(joke6);
+    _jokes.push_back(joke7);
+    return ;
 }
 
 Bot::~Bot(void)
